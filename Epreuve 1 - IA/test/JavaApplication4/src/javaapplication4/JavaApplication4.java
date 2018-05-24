16accb8d43d9c810bb954d11a654e9dfbe11af88
yv71 <33083095+yv71@users.noreply.github.com>
2018-05-24 18:20:06 +0200

    test

--- Epreuve 1 - IA/test/JavaApplication4/src/javaapplication4/JavaApplication4.java
+++ Epreuve 1 - IA/test/JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -17,6 +17,7 @@ public class JavaApplication4 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello");
+        System.out.println("salutcon");
     }
     
 }
