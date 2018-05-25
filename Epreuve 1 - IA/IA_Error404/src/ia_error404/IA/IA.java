568c266487da2b0e53e4eaf9cc1991409120c703
yv71 <33083095+yv71@users.noreply.github.com>
2018-05-25 18:09:31 +0200

    IA

--- Epreuve 1 - IA/IA_Error404/src/ia_error404/IA/IA.java
+++ Epreuve 1 - IA/IA_Error404/src/ia_error404/IA/IA.java
@@ -8,7 +8,9 @@ package ia_error404.IA;
 import ia_error404.Cases.Case;
 import ia_error404.Graphe.Algorithmes.Dijkstra;
 import ia_error404.Map;
+import ia_error404.Objet.Fruit;
 import ia_error404.Team.Lanceur;
+import java.util.HashMap;
 
 /**
  *
@@ -18,11 +20,16 @@ public abstract class IA {
     private Lanceur joueur;
     protected Dijkstra algo;
     protected int turn;
+    protected HashMap<Fruit, Boolean> visited;
     
     public IA(Lanceur _joueur){
         this.joueur = _joueur;
         this.algo = new Dijkstra(this.joueur.getMap().getGraphe());
         this.turn =0;
+        this.visited = new HashMap<>();
+        for (Fruit f : this.getMap().getListeFruitsOK()){
+            visited.put(f,false);
+        }
     }
     
     public Map getMap(){
