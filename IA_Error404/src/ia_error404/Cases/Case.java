b5901e8ecc0413b990d63c98dcc2ac828ab030c9
yv71 <33083095+yv71@users.noreply.github.com>
2018-05-25 14:50:01 +0200

    First step

---
+++ IA_Error404/src/ia_error404/Cases/Case.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ia_error404.Cases;
+
+/**
+ *
+ * @author Beelzed
+ */
+public abstract class Case {
+    private int x;
+    private int y;
+    
+}
